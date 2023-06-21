f51201e781ccb902911717ad3466ca4216a604fd
naru-99 <naru99yoneda@gmail.com>
2023-06-21 04:45:37 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lib763',
+    version="0.0.1",
+    description="naru's library",
+    long_description="",
+    author='naru',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ]
+)
