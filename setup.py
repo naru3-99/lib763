198b99fc54b914ac5030ab871e3c8acc68eb8977
naru-99 <naru99yoneda@gmail.com>
2023-06-21 15:03:19 +0900

    save

--- setup.py
+++ setup.py
@@ -4,10 +4,7 @@ setup(
     name='lib763',
     version="0.0.1",
     description="naru's library",
-    long_description="",
     author='naru',
     license='MIT',
-    classifiers=[
-        "Development Status :: 1 - Planning"
-    ]
+    packages=find_packages()
 )
