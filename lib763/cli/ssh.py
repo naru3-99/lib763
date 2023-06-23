71126e7974d855910ea6cc114711e61603a8d667
naru-99 <naru99yoneda@gmail.com>
2023-06-23 18:31:18 +0900

    sshのポートの変更に対応

--- lib763/cli/ssh.py
+++ lib763/cli/ssh.py
@@ -2,17 +2,19 @@ from lib763.cli.cli_operator import cli_operator
 
 
 class ssh_operator:
-    def __init__(self, user_name: str, host_name: str, key_path: str) -> None:
+    def __init__(self, user_name: str, host_name: str, key_path: str, port: int = 22) -> None:
         """
         @param:
             user_name: (str) SSH接続のユーザー名
             host_name: (str) SSH接続先のホスト名
             key_path: (str) SSH秘密鍵のファイルパス
+            port: (int) SSH接続のポート番号（デフォルトは22）
         """
         self.cmd = cli_operator()
         self.__user = user_name
         self.__host = host_name
         self.__key_path = key_path
+        self.__port = port
         self.__state = False
 
     def set_state(self, flag: bool) -> None:
@@ -35,7 +37,7 @@ class ssh_operator:
         """
         SSH接続を初期化する
         """
-        self.cmd.execute(f"ssh -i {self.__key_path} {self.__user}@{self.__host}")
+        self.cmd.execute(f"ssh -i {self.__key_path} -p {self.__port} {self.__user}@{self.__host}")
         self.__state = True
 
     def execute(self, command: str) -> tuple:
