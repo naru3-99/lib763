594d43432f2ea7d06a5d6d3d64deadf1fb273c45
Naru <126166523+naru-99@users.noreply.github.com>
2023-06-26 17:03:47 +0900

    ファイルが無かったときの処理を追加

--- lib763/fs/save_load.py
+++ lib763/fs/save_load.py
@@ -29,6 +29,9 @@ def load_pickle(path: str) -> object:
         object: 保存されていたオブジェクト
     指定したパスのpickleファイルからオブジェクトを読み込みます。
     """
+    if not os.path.exists(path):
+        print(f"error: no such file {path}")
+        return
     with open(path, "rb") as f:
         obj = pickle.load(f)
     return obj
@@ -75,12 +78,13 @@ def __load_strls(path: str, encoding="utf-8") -> list:
     指定したパスに保存された文字列を行ごとに取得します。
     パスが存在しない場合はNoneを返します。
     """
-    try:
-        with open(path, "r", encoding=encoding) as f:
-            strls = f.readlines()
-        return [row.replace("\n", "") for row in strls]
-    except:
-        return None
+    if not os.path.exists(path):
+        print(f"error: no such file {path}")
+        return
+    with open(path, "r", encoding=encoding) as f:
+        strls = f.readlines()
+    return [row.replace("\n", "") for row in strls]
+
 
 
 def load_sentence(path: str, encoding="utf-8") -> str:
