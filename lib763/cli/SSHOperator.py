3d30d4d21085dc52c8d3124eca916144c6b4a553
naru-99 <naru99yoneda@gmail.com>
2023-06-29 14:20:37 +0900

    ver 0.1.11 sshにget ssh stateメゾットを実装

--- lib763/cli/SSHOperator.py
+++ lib763/cli/SSHOperator.py
@@ -12,68 +12,97 @@ class SSHOperator:
         port: int = 22,
     ):
         """
-        初期化処理
-        :param username: SSH接続のユーザー名
-        :param hostname: SSH接続のホスト名
-        :param password: SSH接続のパスワード
-        :param key_path: SSHの秘密鍵のファイルパス
-        :param port: SSH接続のポート番号 (デフォルトは22)
+        SSHOperatorクラスの初期化メソッド。
+
+        Args:
+            username (str): SSH接続のユーザー名。
+            hostname (str): SSH接続のホスト名。
+            password (str): SSH接続のパスワード。
+            key_path (str, optional): SSHの秘密鍵のファイルパス。デフォルトはNone。
+            port (int, optional): SSH接続のポート番号。デフォルトは22。
+
+        Raises:
+            SSHConnectionError: SSH接続が失敗した場合に発生。
         """
-        self.username = username
-        self.hostname = hostname
-        self.password = password
-        self.key_path = key_path
-        self.port = port
-        self.client = paramiko.SSHClient()
-        self.client.set_missing_host_key_policy(
-            paramiko.AutoAddPolicy()
-        ) 
+        self._username = username
+        self._hostname = hostname
+        self._password = password
+        self._key_path = key_path
+        self._port = port
+        self._client = paramiko.SSHClient()
+        self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self._init_ssh()
 
     def _init_ssh(self):
         """
-        SSH接続を初期化する。
+        SSH接続を初期化するプライベートメソッド。
+
+        Raises:
+            SSHConnectionError: SSH接続が失敗した場合に発生。
         """
-        self.client.connect(
-            self.hostname,
-            username=self.username,
-            password=self.password,
-            key_filename=self.key_path,
-            port=self.port,
-        )
+        try:
+            self._client.connect(
+                self._hostname,
+                username=self._username,
+                password=self._password,
+                key_filename=self._key_path,
+                port=self._port,
+            )
+        except Exception as e:
+            raise SSHConnectionError("SSH connection failed.") from e
 
     def execute(self, command: str) -> str:
         """
-        コマンドを実行する。
-        :param command: 実行するコマンド
-        :return: コマンドの出力
+        SSH接続を通じてリモートコマンドを実行する。
+
+        Args:
+            command (str): 実行するコマンド。
+
+        Returns:
+            str: コマンドの実行結果。
+
+        Raises:
+            SSHConnectionError: SSH接続が有効でない場合に発生。
         """
-        if not self.client.get_transport().is_active():
+        if not self.get_ssh_state():
             raise SSHConnectionError("SSH接続が有効ではありません。")
-        stdin, stdout, stderr = self.client.exec_command(command)
+        stdin, stdout, stderr = self._client.exec_command(command)
         return stdout.read().decode("utf-8")
 
     def send_file(self, local_path: str, remote_path: str):
         """
         ファイルをリモートシステムに送信する。
-        :param local_path: ローカルのファイルパス
-        :param remote_path: リモートのファイルパス
+
+        Args:
+            local_path (str): ローカルのファイルパス。
+            remote_path (str): リモートのファイルパス。
+
+        Raises:
+            SSHConnectionError: SSH接続が有効でない場合に発生。
         """
-        if not self.client.get_transport().is_active():
+        if not self.get_ssh_state():
             raise SSHConnectionError("SSH接続が有効ではありません。")
-        with SCPClient(self.client.get_transport()) as scp:
+        with SCPClient(self._client.get_transport()) as scp:
             scp.put(local_path, remote_path)
+
+    def get_ssh_state(self) -> bool:
+        """
+        現在のSSH接続の状態を確認します。
+
+        Returns:
+            bool: SSH接続が有効な場合はTrue、そうでない場合はFalse。
+        """
+        return self._client.get_transport().is_active()
 
     def exit(self):
         """
         SSH接続を閉じる。
         """
-        self.client.close()
+        self._client.close()
 
 
 class SSHConnectionError(Exception):
     """
     SSH接続エラーを処理するためのカスタム例外クラス。
     """
-
     pass
