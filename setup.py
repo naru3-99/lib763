69ad0dd2c1507d5dbf611172c3113a0e347716a7
naru-99 <naru99yoneda@gmail.com>
2023-07-11 15:47:48 +0900

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,14 @@ setup(
     description="naru's library",
     author="naru",
     license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "paramiko",
+        "chardet",
+        "opencv-python",  # cv2 is provided by opencv-python
+        "keyboard",
+        "mouse",
+        "pyperclip",
+        "pyautogui",
+    ],
 )
