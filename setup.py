9cd0d409cf79b5441cf38b0c1a80ae6f7341cf93
naru-99 <naru99yoneda@gmail.com>
2023-07-30 18:45:06 +0900

    1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lib763",
-    version="1.1",
+    version="1.2",
     description="naru's library",
     author="naru",
     license="MIT",
