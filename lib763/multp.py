2c6e230422400a1f1a209208f1c52b44f129cdbe
naru3-99 <naru99yoneda@gmail.com>
2024-09-10 15:27:31 +0900

    不要なファイルを削除した

--- lib763/multp.py
+++ lib763/multp.py
@@ -1,6 +1,5 @@
 import multiprocessing as mp
 import concurrent.futures
-import time
 from typing import Any, Callable, List, Union
 
 
@@ -20,6 +19,10 @@ def start_process(
     process = mp.Process(target=target, args=args)
     process.start()
     return process
+
+
+def is_process_alive(process: mp.Process) -> bool:
+    return process.is_alive()
 
 
 def stop_process(process: mp.Process) -> None:
@@ -47,106 +50,3 @@ def parallel_process(func: Callable[..., Any], data_list: List[Any]) -> List[Any
     with concurrent.futures.ProcessPoolExecutor() as executor:
         results = executor.map(func, data_list)
     return list(results)
-
-
-class EventHandler:
-    def __init__(self) -> None:
-        """
-        Initializes a new Event object.
-        """
-        self._event = mp.Event()
-        self.cur_event_type = mp.Value("i", -1)
-        # dict[type] = handlers_list
-        self.type_handlers_dict = {}
-
-    def set_event(self, event_type: int = None):
-        """
-        Sets the event and calls the registered handlers.
-
-        Parameters:
-            event_type (int): The type of the event.
-                              If specified, the current event type will be set to this value.
-        """
-        if event_type is not None and not isinstance(event_type, int):
-            raise ValueError("event_type must be an integer or None")
-        if not event_type is None:
-            self.cur_event_type.value = event_type
-            if not event_type in self.type_handlers_dict.keys():
-                self.type_handlers_dict[event_type] = []
-        self._event.set()
-        self._call_handlers()
-
-    def get_current_event_type(self):
-        """
-        Returns the current event type.
-        """
-        return self.cur_event_type.value
-
-    def clear_event(self, event_type: int = None):
-        """
-        Clears the event and resets the current event type.
-        Parameters:
-        event_type (int): The type of the event.
-                          If specified, the current event type will be set to this value.
-        """
-        if event_type is not None and not isinstance(event_type, int):
-            raise ValueError("event_type must be an integer or None")
-
-        if not event_type == None:
-            if event_type != self.cur_event_type.value:
-                print(f"current event is {self.cur_event_type.value}, not {event_type}")
-                return
-        self._event.clear()
-        self.cur_event_type.value = -1
-
-    def wait(self, event_type: int = None, timeout: float = None):
-        """
-        Waits for the event to be set.
-
-        Parameters:
-            event_type (int): The type of the event to wait for. If None, waits for any event to be set.
-            timeout (float): The maximum time to wait for the event to be set.
-        Returns:
-            bool: True if the event was set, False otherwise.
-        """
-        if event_type is not None and not isinstance(event_type, int):
-            raise ValueError("event_type must be an integer or None")
-
-        if event_type is None:
-            return self._event.wait(timeout)
-
-        start_time = time.time()
-
-        while True:
-            if not timeout is None:
-                if time.time() - start_time > timeout:
-                    return False
-            if self.get_current_event_type() == event_type:
-                return True
-            time.sleep(0.2)
-
-    def register_handler(self, event_type: int, handler: Callable):
-        """
-        Registers a new handler for the specified event type.
-
-        Parameters:
-            event_type (int): The type of the event.
-            handler (Callable): The handler function to be called when the event is set.
-        """
-        if event_type is not None and not isinstance(event_type, int):
-            raise ValueError("event_type must be an integer or None")
-
-        if not callable(handler):
-            raise TypeError("handler must be Callable")
-
-        if not event_type is None:
-            if not event_type in self.type_handlers_dict.keys():
-                self.type_handlers_dict[event_type] = []
-        self.type_handlers_dict[event_type].append(handler)
-
-    def _call_handlers(self):
-        """
-        Calls all registered handlers for the current event type.
-        """
-        for func in self.type_handlers_dict[self.cur_event_type.value]:
-            func()
