d5b0f63f0c8b905d56b7e7a7c25dead7d668475e
naru3-99 <naru99yoneda@gmail.com>
2024-11-06 12:59:20 +0900

    英語論文をコピペするとき用の関数を追加した。

--- lib763/regex.py
+++ lib763/regex.py
@@ -1,5 +1,6 @@
 import re
 from typing import Dict, Pattern, List
+from pyperclip import copy, paste
 
 # 数字に一致するパターン
 PATTERN_DIGIT = r"\d"
@@ -73,3 +74,15 @@ def contains_substring(input_string: str, substring: str) -> bool:
     """
     pattern = re.escape(substring)
     return re.search(pattern, input_string) is not None
+
+
+def mold_copied_eng_paper(text):
+    text1 = re.sub(r"-[\r\n]+", "", text)
+    text2 = re.sub(r"[\r\n]+[\r\n]+", " ", text1)
+    text3 = re.sub(r"[\r\n]+", " ", text2)
+    return text3
+
+
+def mold_eng_from_clipboard():
+    text = paste()
+    copy(mold_copied_eng_paper(text))
